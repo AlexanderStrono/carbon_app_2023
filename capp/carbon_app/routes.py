909521bc064df0d66dbd22413f1328cd980c23d6
AlexanderStrono <126338894+AlexanderStrono@users.noreply.github.com>
2023-04-21 09:17:07 +0200

    Add files via upload

---
+++ capp/carbon_app/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template, Blueprint
+from capp.models import Transport
+from flask_login import login_required
+
+carbon_app=Blueprint('carbon_app',__name__)
+
+@carbon_app.route('/carbon_app')
+def carbon_app_home():
+    return render_template('carbon_app/carbon_app.html', title='carbon_app')
+
+@carbon_app.route('/carbon_app/new_entry')
+@login_required
+def new_entry_home():
+    return render_template('carbon_app/new_entry.html', title='new_entry')
+
+@carbon_app.route('/carbon_app/your_data')
+@login_required
+def your_data_home():
+    return render_template('carbon_app/your_data.html', title='your_data')
+    
